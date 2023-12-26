2596df968164bbfd28b372710d0956846676f867
ZuhairTarif <20-42115-1@student.aiub.edu>
2023-12-26 22:49:12 +0600

    Adaptation Done

--- Frontend/increaseScript.js
+++ Frontend/increaseScript.js
@@ -1,10 +1,55 @@
+function getCurrentSize(element) {
+    return parseFloat(window.getComputedStyle(element, null).getPropertyValue('font-size'));
+}
+
+function increaseSizeElements() {
+    var elementsToResize = document.querySelectorAll('.clickable');
+    elementsToResize.forEach(function(element) {
+        var currentSize = getCurrentSize(element);
+        var newSize = currentSize * 1.1; 
+        element.style.fontSize = newSize + 'px';
+        localStorage.setItem('appliedFontSize', newSize.toString());
+    });
+}
+
+function applyStoredFontSize() {
+    var storedSize = localStorage.getItem('appliedFontSize');
+    if (storedSize) {
+        var elementsToResize = document.querySelectorAll('.clickable');
+        elementsToResize.forEach(function(element) {
+            element.style.fontSize = parseFloat(storedSize) + 'px';
+        });
+    }
+}
+
+function checkAndIncreaseSize() {
+    var increaseData = localStorage.getItem('increaseData');
+    var currentIncreaseSize = 0;
+
+    if (increaseData) {
+        var parsedData = JSON.parse(increaseData);
+        if (parsedData && parsedData.increaseSize) {
+            currentIncreaseSize = parseFloat(parsedData.increaseSize);
+        }
+    }
+
+    var previousIncreaseSizeString = localStorage.getItem('previousIncreaseSize');
+    var previousIncreaseSize = previousIncreaseSizeString ? parseFloat(previousIncreaseSizeString) : null;
+
+    if (previousIncreaseSize !== null && currentIncreaseSize > previousIncreaseSize) {
+        increaseSizeElements();
+    } else {
+        applyStoredFontSize(); 
+    }
+
+    localStorage.setItem('previousIncreaseSize', currentIncreaseSize.toString());
+}
+
+document.addEventListener('DOMContentLoaded', checkAndIncreaseSize);
 
 document.addEventListener('DOMContentLoaded', function() {
-
-    if (localStorage.getItem('page_open_count')) {
-        localStorage.setItem('page_open_count', parseInt(localStorage.getItem('page_open_count')) + 1);
-    } else {
-        localStorage.setItem('page_open_count', 1);
+    var closeButton = document.getElementById('closeButton');
+    if (closeButton) {
+        closeButton.addEventListener('click', checkAndIncreaseSize);
     }
-    console.log('Page has been opened ' + localStorage.getItem('page_open_count') + ' times.');
 });
